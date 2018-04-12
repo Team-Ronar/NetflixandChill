cc5114ce0cab7b44ae6fadd9cebd1050825c1d06
Peter Horton <nardos.n9@gmail.com>
2018-04-12 04:40:37 +0000

    none

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -13,7 +13,46 @@ export class SearchPage {
 
   currentItems: any = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public items: Items) { }
+  
+    
+    // let val = ev.target.value;
+    // if (!val || !val.trim()) {
+    //   this.currentItems = [];
+    //   return;
+    // }
+    // this.currentItems = this.items.query({
+    //   name: val
+    // });
+  }
+
+  /**
+   * Navigate to the detail page for this item.
+   */
+  openItem(item: Item) {
+    this.navCtrl.push('ItemDetailPage', {
+      item: item
+    });
+   this.api.get(ev.value)
+   .subscribe((data :any) => data.results.forEach(movie => console.log(movie.title)));
+    
+    
+    // let val = ev.target.value;
+    // if (!val || !val.trim()) {
+    //   this.currentItems = [];
+    //   return;
+    // }
+    // this.currentItems = this.items.query({
+    //   name: val
+    // });
+  }
+
+  /**
+   * Navigate to the detail page for this item.
+   */
+  openItem(item: Item) {
+    this.navCtrl.push('ItemDetailPage', {
+      item: item
+    });Params, public items: Items) { }
 
   /**
    * Perform a service for the proper items.
