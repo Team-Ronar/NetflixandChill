900c21d29199968f64b7abfe7d12c7b2fb031cb5
Robert Brown <robertlb4@gmail.com>
2018-04-17 01:56:55 +0000

    changed page order

--- src/pages/content/content.ts
+++ src/pages/content/content.ts
@@ -17,24 +17,23 @@ export class ContentPage {
     loop: true,
   }
   
-  
 
   constructor(public api: Api, public navCtrl: NavController) { }
 
-getNowPlaying() {
+  getNowPlaying() {
     let endpoint = 'movie/now_playing';
     this.api.get(endpoint, {api_key: this.api.apiKey, language: 'en-US'})
     .subscribe( (result: any) => result.results.forEach( movie => this.slides.push(movie.poster_path)));
-};
+  };
 
-ionViewDidLoad() {
-  let endpoint = 'movie/now_playing';
+  ionViewDidLoad() {
+   let endpoint = 'movie/now_playing';
     this.api.get(endpoint, {api_key: this.api.apiKey, language: 'en-US'})
-    .subscribe(result => {
+    .subscribe((result: any) => {
       result.results.forEach( movie => this.slides.push(movie.poster_path))
       console.log(this.slides);
       });
-};
+  };
 
 
 }
